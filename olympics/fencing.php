5f516872aeb5445ab424c8601d1d94bd1721173d
Andor Salamon <salamon.andor@gmail.com>
2019-08-16 14:47:30 +0200

    Add update Signed-off-by: Andor Salamon <salamon.andor@gmail.com>

--- olympics/fencing.php
+++ olympics/fencing.php
@@ -1,5 +1,16 @@
 <?php
 include('../config.php');
+if (isset($_POST["name"])) 
+{
+	$name = $_POST["name"];
+	$sql = "SELECT c.flag, c.name, fn.gold, fn.silver, fn.bronze, fn.total FROM countries c INNER JOIN fencing fn ON c.id=fn.country_id WHERE c.name LIKE '%$name%' ORDER BY fn.gold DESC, fn.silver DESC, fn.bronze DESC";
+	$result = mysqli_query($connection, $sql);
+}
+else 
+{
+	$sql = "SELECT c.flag, c.name, fn.gold, fn.silver, fn.bronze, fn.total FROM countries c INNER JOIN fencing fn ON c.id=fn.country_id ORDER BY fn.gold DESC, fn.silver DESC, fn.bronze DESC";
+	$result = mysqli_query($connection, $sql);
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -24,6 +35,16 @@ include('../config.php');
 		</div>
 		<div class="row">
 			<div class="col">
+				<form method="post" action="">
+					<div class="form-group">
+						<label for="name">Country Name:</label>
+						<input type="text" class="form-control" name="name" id="name" placeholder="Enter Country Name">
+					</div>
+				</form>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col">
 				<table class="table" align="center">
 					<thead>
 						<tr>
@@ -37,8 +58,6 @@ include('../config.php');
 					</thead>
 					<tbody>
 						<?php
-						$sql = "SELECT c.flag, c.name, fn.gold, fn.silver, fn.bronze, fn.total FROM countries c INNER JOIN fencing fn ON c.id=fn.country_id ORDER BY fn.gold DESC, fn.silver DESC, fn.bronze DESC";
-						$result = mysqli_query($connection, $sql);
 						if (mysqli_num_rows($result) > 0) 
 						{
 							while ($row = mysqli_fetch_array($result)) 
